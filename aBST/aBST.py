a77b57fff4af9e32c1cd842a7837d00fff106d35
klaus <70817966+klaus-wq@users.noreply.github.com>
2025-01-25 00:53:39 +0700

    add task

---
+++ aBST/aBST.py
@@ -0,0 +1,31 @@
+
+
+class aBST:
+
+    def __init__(self, depth):
+        # правильно рассчитайте размер массива для дерева глубины depth:
+        tree_size = pow(2, depth + 1) - 1
+        self.Tree = [None] * tree_size  # массив ключей
+
+    def FindKeyIndex(self, key):
+        # ищем в массиве индекс ключа
+        self.FindKeyIndexRecursive(key, 0)
+
+    def FindKeyIndexRecursive(self, key, index):
+        if index >= len(self.Tree):
+            return None
+        if self.Tree[index] is None:
+            return index * (-1)
+        if self.Tree[index] == key:
+            return index
+        if self.Tree[index] > key:
+            return self.FindKeyIndexRecursive(key, 2 * index + 1)
+        return self.FindKeyIndexRecursive(key, 2 * index + 2)
+
+    def AddKey(self, key):
+        index = self.FindKeyIndex(key)
+        if index is None:
+            return -1
+        if index < 0:
+            self.Tree[abs(index)] = key
+        return abs(index)
